2107faa8df9f08ec7d7c373d893c293c2ff5ceba
suecoalto <suecoalto@gmail.com>
2025-10-14 01:40:20 +0200

    Mongodb connection added
    Establishing connection to database using connectDB function from config/db.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
+
+dotenv.config({ debug: true });
 
 const app = express();
-const port = 5001;
+const port = process.env.PORT || 5000;
 
+// Express
 app.listen(port, () => {
 	console.log(`Server started on port ${port}`);
 });
 
+//Mongodb connection
+connectDB();
